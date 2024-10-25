b6d26bf2e846451345386364d78a8a2b58582f76
TrueSunGaming <truesungaming@gmail.com>
2024-10-25 13:47:06 +0000

    feat: create regexes for tokens

--- src/compiler/tokenizer/TokenType.hpp
+++ src/compiler/tokenizer/TokenType.hpp
@@ -1,23 +1,19 @@
 #pragma once
 
 enum TokenType {
-    EOL,
-    IDENTIFIER,
-    INTEGER,
-    FLOAT,
-    STRING,
-    FSTRING_QUOTE,
-    FSTRING_TEXT,
-    FSTRING_FORMAT_OPEN,
-    FSTRING_FORMAT_CLOSE,
-    PARENTHESIS_OPEN,
-    PARENTHESIS_CLOSE,
-    SQUARE_OPEN,
-    SQUARE_CLOSE,
-    CURLY_OPEN,
-    CURLY_CLOSE,
-    COMMA,
-    COLON,
-    OPERATOR,
-    QUESTION
+    EOL, // ;
+    IDENTIFIER, // /[a-z_$][\w$]*/gi
+    INTEGER, // /\d+(e\+?\d+)?/g
+    FLOAT, // /(\d+\.(\d*)?(e(-|\+)?\d+(\.\d*)?)?)|(\d+(\.\d*)?e-\d+(\.\d*)?)/g
+    STRING, // custom implementation
+    PARENTHESIS_OPEN, // (
+    PARENTHESIS_CLOSE, // )
+    SQUARE_OPEN, // [
+    SQUARE_CLOSE, // ]
+    CURLY_OPEN, // {
+    CURLY_CLOSE, // }
+    COMMA, // ,
+    COLON, // :
+    OPERATOR, // 
+    QUESTION // ?
 };

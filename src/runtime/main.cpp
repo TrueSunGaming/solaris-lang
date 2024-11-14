12c8c46317e3b734cf1450c25c465bd5f2c80b6e
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-11-14 18:49:46 -0500

    fix: include stdexcept in runtime

--- src/runtime/main.cpp
+++ src/runtime/main.cpp
@@ -1,4 +1,5 @@
 #include "state/RuntimeState.hpp"
+#include <stdexcept>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) throw std::runtime_error("Please provide an assembly file as an argument");

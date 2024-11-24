d3c2b17165328b60f9a7aa157cca81f6ab03f5cd
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-11-23 21:58:22 -0500

    fix: call Object constructor in Function constructor

--- src/runtime/data/function/Function.cpp
+++ src/runtime/data/function/Function.cpp
@@ -1,5 +1,6 @@
 #include "Function.hpp"
 
 Function::Function(Object *returnObj, std::vector<FunctionArgument> args) :
+    Object(ValueType::FUNCTION),
     returnObj(returnObj),
     args(args) {}
